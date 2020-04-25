3e579ef750e40c4983e76f20b9a516aff6e4ede0
BickySamal <34908641+BickySamal@users.noreply.github.com>
2020-04-25 19:17:34 +0530

    Add files via upload

---
+++ QuizScript2.js
@@ -0,0 +1,25 @@
+var counter = 0;
+//Page();
+function Page()
+{
+	counter++;
+	alert(counter);
+}
+//counter = Page.counter;
+//this.counter =  counter;
+//Page()
+//	Page.counter=0;
+	
+function Submit()
+{
+	//document.write("d");
+	if(counter==5)
+		document.write("We are CHADDI-BUDDY (*_*) ");
+	else if(counter == 4)
+		document.write("You got is bro...^_^");
+	else if(counter == 3)
+		document.write("Not BAD ;) ");
+	else
+		document.write("You don't know anythiing :'( ");
+	
+}
